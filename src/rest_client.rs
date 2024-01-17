cd3e8f0f4761f4af943465391b16fed97e012f28
Diego Yegros <diegoyegrosa@gmail.com>
2024-01-17 12:32:36 -0300

    fix consume rest api

--- src/rest_client.rs
+++ src/rest_client.rs
@@ -1,14 +1,14 @@
 use reqwest::blocking::get;
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     const URL: &str = "https://cataas.com/cat";
     let response = get(URL)?;
     let status_code = response.status();
-    let img_data = response.text()?;
+    let img_data = response.bytes()?;
     println!("Response from {} is {}", URL, status_code.to_string());
-    let output_filename = format!("image-{}.jpg", chrono::Utc::now().format("%Y%m%d-%H%M%S"));
+    let output_filename = format!("image-{}.jpeg", chrono::Utc::now().format("%H%M%S"));
     let exe_path = env::current_exe().unwrap();
     let base_dir = exe_path
         .ancestors()
@@ -19,7 +19,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .to_owned()
         + "\\data\\";
     let mut out_file = File::create(base_dir.to_owned() + &output_filename)?;
-    out_file.write_all(img_data.as_bytes())?;
+    out_file.write_all(&img_data)?;
 
     println!("Image saved to '{}'", output_filename);
     Ok(())
